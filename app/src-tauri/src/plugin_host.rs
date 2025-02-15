b6364ab072c2d155f1624ad666d50b3aad770bb6
zty012 <me@zty012.de>
2025-02-15 13:34:14 +0800

    :sparkles: Add PluginHost

--- app/src-tauri/src/plugin_host.rs
+++ app/src-tauri/src/plugin_host.rs
@@ -26,19 +26,3 @@ struct Plugin {
     author: String,
     entry_point: Path,
 }
-
-impl serde::Serialize for Plugin {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: serde::Serializer,
-    {
-        let mut state = serializer.serialize_struct("Plugin", 6)?;
-        state.serialize_field("id", &self.id)?;
-        state.serialize_field("name", &self.name)?;
-        state.serialize_field("version", &self.version)?;
-        state.serialize_field("description", &self.description)?;
-        state.serialize_field("author", &self.author)?;
-        state.serialize_field("entry_point", &self.entry_point.to_str().unwrap())?;
-        state.end()
-    }
-}
