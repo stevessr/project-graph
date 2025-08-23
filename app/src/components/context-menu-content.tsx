3950627a50fc3324c6ba615263beee5c0c15e7ca
littlefean <2028140990@qq.com>
2025-08-23 18:50:02 +0800

    ✨ 右键菜单中增加鼠标模式切换

--- app/src/components/context-menu-content.tsx
+++ app/src/components/context-menu-content.tsx
@@ -35,20 +35,24 @@ import {
   Copy,
   Dot,
   Grip,
+  MousePointer,
   MoveHorizontal,
   Network,
   Package,
   Palette,
+  Pencil,
   Scissors,
   Slash,
   SquareRoundCorner,
   TextSelect,
   Trash,
+  Waypoints,
 } from "lucide-react";
 import { useTranslation } from "react-i18next";
 import { toast } from "sonner";
 import tailwindColors from "tailwindcss/colors";
 import KeyTooltip from "./key-tooltip";
+import { Settings } from "@/core/service/Settings";
 
 const Content = ContextMenuContent;
 const Item = ContextMenuItem;
@@ -57,6 +61,10 @@ const SubTrigger = ContextMenuSubTrigger;
 const SubContent = ContextMenuSubContent;
 // const Separator = ContextMenuSeparator;
 
+/**
+ * 右键菜单
+ * @returns
+ */
 export default function MyContextMenuContent() {
   const [p] = useAtom(activeProjectAtom);
   const { t } = useTranslation("contextMenu");
@@ -69,6 +77,7 @@ export default function MyContextMenuContent() {
 
   return (
     <Content>
+      {/* 第一行 */}
       <Item className="bg-transparent! gap-0 p-0">
         <KeyTooltip keyId="copy">
           <Button variant="ghost" size="icon" onClick={() => p.copyEngine.copy()}>
@@ -297,6 +306,7 @@ export default function MyContextMenuContent() {
           )}
         </div>
       </Item>
+      {/* 存在选中实体 */}
       {p.stageManager.getSelectedEntities().length > 0 &&
         p.stageManager.getSelectedEntities().every((it) => "color" in it) && (
           <Sub>
@@ -325,6 +335,7 @@ export default function MyContextMenuContent() {
             </SubContent>
           </Sub>
         )}
+      {/* 存在两个及以上选中实体 */}
       {p.stageManager.getSelectedEntities().length >= 2 && (
         <>
           <Item onClick={() => p.stageManager.packEntityToSectionBySelected()}>
@@ -384,6 +395,7 @@ export default function MyContextMenuContent() {
           </Item>
         </>
       )}
+      {/* 存在选中 TextNode */}
       {p.stageManager.getSelectedEntities().filter((it) => it instanceof TextNode).length > 0 && (
         <>
           <Item
@@ -399,6 +411,7 @@ export default function MyContextMenuContent() {
           </Item>
         </>
       )}
+      {/* 存在选中 Section */}
       {p.stageManager.getSelectedEntities().filter((it) => it instanceof Section).length > 0 && (
         <>
           <Item onClick={() => p.stageManager.sectionSwitchCollapse()}>
@@ -407,6 +420,36 @@ export default function MyContextMenuContent() {
           </Item>
         </>
       )}
+      {/* 鼠标模式 */}
+      <Item className="bg-transparent! gap-0 p-0">
+        <Button
+          variant="ghost"
+          size="icon"
+          onClick={() => {
+            Settings.mouseLeftMode = "selectAndMove";
+          }}
+        >
+          <MousePointer />
+        </Button>
+        <Button
+          variant="ghost"
+          size="icon"
+          onClick={() => {
+            Settings.mouseLeftMode = "draw";
+          }}
+        >
+          <Pencil />
+        </Button>
+        <Button
+          variant="ghost"
+          size="icon"
+          onClick={() => {
+            Settings.mouseLeftMode = "connectAndCut";
+          }}
+        >
+          <Waypoints />
+        </Button>
+      </Item>
     </Content>
   );
 }
