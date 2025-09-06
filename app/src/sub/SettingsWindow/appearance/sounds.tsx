06266c0f2ed199682e7f44b999fe28d48d581960
littlefean <2028140990@qq.com>
2025-09-06 19:30:10 +0800

    doc: 添加音效文档链接

--- app/src/sub/SettingsWindow/appearance/sounds.tsx
+++ app/src/sub/SettingsWindow/appearance/sounds.tsx
@@ -1,9 +1,11 @@
 import { Settings } from "@/core/service/Settings";
 import { SoundService } from "@/core/service/feedbackService/SoundService";
-import { Volume2, VolumeX } from "lucide-react";
+import { ExternalLink, Volume2, VolumeX } from "lucide-react";
 import { useTranslation } from "react-i18next";
 import { Switch } from "@/components/ui/switch";
 import FileChooser from "@/components/ui/file-chooser";
+import { Popover } from "@/components/ui/popover";
+import { open } from "@tauri-apps/plugin-shell";
 
 // 音效配置列表
 const SOUND_CONFIGS = [
@@ -39,6 +41,24 @@ export default function SoundEffectsPage() {
 
   return (
     <div className="space-y-4">
+      <p>提示：目前此页面有一个bug：需要切换一下页面再切回来，才能看到改动的效果</p>
+      <Popover.Confirm
+        title="提示"
+        description="即将跳转github页面。如果github页面无法打开，请自行解决或使用自定义音效。"
+        onConfirm={() => open("https://github.com/LiRenTech/project-graph-cdn")}
+      >
+        <div className="bg-muted/50 **:cursor-pointer group flex flex-1 cursor-pointer flex-col justify-center gap-2 rounded-lg border p-4">
+          <div className="flex items-center justify-center gap-2">
+            <ExternalLink className="h-5 w-5" />
+            <span className="text-lg">前往官方音效库Github仓库:</span>
+          </div>
+          <div className="flex items-end justify-center gap-2 text-center">
+            <span className="underline-offset-4 group-hover:underline">
+              https://github.com/LiRenTech/project-graph-cdn
+            </span>
+          </div>
+        </div>
+      </Popover.Confirm>
       <div className="bg-muted flex items-center justify-between rounded-lg p-4">
         <div className="flex items-center gap-2">
           {soundEnabled ? <Volume2 /> : <VolumeX />}
@@ -89,7 +109,6 @@ export default function SoundEffectsPage() {
           <p>{t("soundDisabledHint")}</p>
         </div>
       )}
-      <p>提示：目前此页面有一个bug：需要切换一下页面再切回来，才能看到改动的效果</p>
     </div>
   );
 }
